ae14c90bae11b5a702ec0dec9f042fd96211b680
ivannemeth <nemethivn@gmail.com>
2024-01-16 15:20:09 +0100

    get added note to the homepage

--- components/NotesList.jsx
+++ components/NotesList.jsx
@@ -13,22 +13,20 @@ export default function NotesList() {
   }
 
   return (
-    <p>hi</p>
+    <>
+      {data.map((note) => (
+        <div key={note._id}>
+          <div>
+            <h2>{note.title}</h2>
+            <div>{note.description}</div>
+          </div>
 
-    /* {data.map((note) => (
-      <div key={note._id} >
-        
-        <div>
-          <h2>{note.title}</h2>
-          <div>{note.description}</div>
+          <div>
+            <DeleteButton id={note._id} />
+            <Link href="/api/notes/ABC"></Link>
+          </div>
         </div>
-
-        <div>
-          <DeleteButton id={note._id} />
-          <Link href="/api/notes/ABC">
-          </Link>
-        </div>
-      </div>
-    ))} */
+      ))}
+    </>
   );
 }
